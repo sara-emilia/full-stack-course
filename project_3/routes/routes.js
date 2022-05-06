30474778150a53a757e76b29e6a3856125144cf8
Emilia <62058192+sara-emilia@users.noreply.github.com>
2022-05-06 16:36:04 +0300

    Create routes.js

---
+++ project_3/routes/routes.js
@@ -0,0 +1,71 @@
+// Use express-module
+const express = require("express");
+// Use Schema model
+const Model = require("../model/model");
+// Use router
+const router = express.Router();
+
+//Add event
+router.post("/add", async (req, res) => {
+  const data = new Model({
+    event_name: req.body.event_name,
+    startdate: req.body.startdate,
+    enddate: req.body.enddate,
+    season: req.body.season,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Get all events
+router.get("/getall", async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get event by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update event by ID
+router.patch("/update/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Delete event by ID
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
